9e113820cbcc387b09883cee35a01b28bcf852e8
Andrew Gee <andrew.gee@gsfgroup.com>
2022-12-31 01:49:16 +0000

    let

--- pages/api/[[...route]].js
+++ pages/api/[[...route]].js
@@ -5,7 +5,7 @@ import axios from 'axios'
 slackApp.createSlackApp()
 
 slackApp.app.event('message', async ({ event, say }) => {
-  const message = event.text
+  let message = event.text
 
   if (!message) {
     return
